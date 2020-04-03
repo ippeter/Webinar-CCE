56806b8cdae4d177f054e2a1e003491480dbca33
Peter Predtechensky <Petr.Predtechensky@gmail.com>
2020-04-03 15:55:25 +0300

    Update to become release candidate

--- webinar.py
+++ webinar.py
@@ -1,6 +1,6 @@
-import os
-import re
-import socket
+import subprocess
+#import re
+#import socket
 import logging
 
 from flask import Flask, jsonify, request, render_template, flash
@@ -9,13 +9,13 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 
 
 class ReusableForm(Form):
-    user_id = TextField('Please enter a message:', validators=[validators.DataRequired()])
+    message = TextField('Please enter a message:', validators=[validators.DataRequired()])
 
 
 # Instantiate our Node
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+#app.config.from_object(__name__)
+#app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 # Enable logging
 LOG = create_logger(app)
@@ -24,25 +24,28 @@ LOG.setLevel(logging.INFO)
 @app.route("/", methods=['GET', 'POST'])
 def handle_input():
     # Get node name and pod IP address, then pass it to the template
-    strNodeName = os.environ['NODE_NAME']
-    strPodIP = os.environ['POD_IP']
+    node_name = subprocess.run(["hostname"])
 
-    flash(strNodeName)
-    flash(strPodIP)
+    # Send node name to the template for rendering
+    flash(node_name)
     
     # Proceed to the form
     form = ReusableForm(request.form)
     print(form.errors)
     
     if (request.method == 'POST'):
-        strUserId = request.form['user_id']
+        message = request.form['message']
  
         if (form.validate()):
             # Handle the message: create a file with a name of the message text without spaces
-            TODO
-            LOG.info("User ID {} received and sent to topic {}, partition {} with offset {}".format(strUserId, mt.topic, mt.partition, mt.offset))
-
-            flash('Thank you. Your message was written to a file!')
+            try:
+                open("".join(message.split()), "w+", encoding='utf-8').close()
+            except:
+                LOG.info("Error creating a file from the message {}".format(message))
+                flash('Epic fail. Something went wrong...')
+            else:
+                LOG.info("Message {} received and written to a file".format(message))
+                flash('Thank you. Your message was written to a file!')
         else:
             flash('Please enter some text. The input field must not be empty.')
  
